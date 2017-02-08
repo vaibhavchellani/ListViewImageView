c367da71eba87d557785a0636d9866f9ba8e1532
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-02-08 23:39:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhavchellani/listviewimageview/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.vaibhavchellani.listviewimageview;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity{
+
+    ListView list;
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
+
+    Integer[] imgid={
+            R.drawable.image2,
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image1,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
